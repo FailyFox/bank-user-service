844b471c1cdda19d834a6219fd3fc05e9ed9e94e
mutieshev <mutieshev@griddynamics.com>
2022-05-12 16:03:35 +0300

    change naming strategy

---
+++ src/main/java/com/greedobank/userservice/CustomPhysicalNamingStrategy.java
@@ -0,0 +1,41 @@
+package com.greedobank.userservice;
+
+import org.hibernate.boot.model.naming.Identifier;
+import org.hibernate.boot.model.naming.PhysicalNamingStrategy;
+import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
+
+
+public class CustomPhysicalNamingStrategy implements PhysicalNamingStrategy {
+
+    @Override
+    public Identifier toPhysicalCatalogName(final Identifier identifier, final JdbcEnvironment jdbcEnv) {
+        return identifier;
+    }
+
+    @Override
+    public Identifier toPhysicalColumnName(final Identifier identifier, final JdbcEnvironment jdbcEnv) {
+        return identifier;
+    }
+
+    @Override
+    public Identifier toPhysicalSchemaName(final Identifier identifier, final JdbcEnvironment jdbcEnv) {
+        return identifier;
+    }
+
+    @Override
+    public Identifier toPhysicalSequenceName(final Identifier identifier, final JdbcEnvironment jdbcEnv) {
+        return identifier;
+    }
+
+    @Override
+    public Identifier toPhysicalTableName(final Identifier identifier, final JdbcEnvironment jdbcEnv) {
+        return firstCharacterToLowerCase(identifier);
+    }
+
+    private Identifier firstCharacterToLowerCase(final Identifier identifier) {
+        String text = identifier.getText();
+        String newTExt = Character.toLowerCase(text.charAt(0)) +
+                (text.length() > 1 ? text.substring(1) : "");
+        return new Identifier(newTExt, false);
+    }
+}

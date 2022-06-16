160ec08ce93f50e5e96cc1d32033632f2182b851
mutieshev <mutieshev@griddynamics.com>
2022-06-16 18:54:28 +0300

    removed unnecessary methods

--- src/main/java/com/greedobank/userservice/controller/ExceptionControllerAdvice.java
+++ src/main/java/com/greedobank/userservice/controller/ExceptionControllerAdvice.java
@@ -1,11 +1,9 @@
 package com.greedobank.userservice.controller;
 
 import com.greedobank.userservice.exception.ExceptionMessage;
-import javax.naming.AuthenticationException;
 import javax.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -15,21 +13,7 @@ public class ExceptionControllerAdvice {
   @ExceptionHandler
   public ResponseEntity<ExceptionMessage> noSuchEntity(EntityNotFoundException exception) {
     return ResponseEntity
-        .status(HttpStatus.BAD_REQUEST)
-        .body(new ExceptionMessage(exception.getMessage()));
-  }
-
-  @ExceptionHandler
-  public ResponseEntity<ExceptionMessage> unauthorized(AuthenticationException exception) {
-    return ResponseEntity
-        .status(HttpStatus.UNAUTHORIZED)
-        .body(new ExceptionMessage(exception.getMessage()));
-  }
-
-  @ExceptionHandler
-  public ResponseEntity<ExceptionMessage> forbidden(AccessDeniedException exception) {
-    return ResponseEntity
-        .status(HttpStatus.FORBIDDEN)
+        .status(HttpStatus.NOT_FOUND)
         .body(new ExceptionMessage(exception.getMessage()));
   }
 }
